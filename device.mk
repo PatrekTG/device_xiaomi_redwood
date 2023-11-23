eb3bb2bf031875c59b897a5c16818b09ae0834ee
thepriyanshujangid <priyanshujangid@yahoo.com>
2023-11-23 10:28:48 +0530

    Drop "[SQUASH] redwood: fastcharge implementation"
    **Not working

--- device.mk
+++ device.mk
@@ -203,10 +203,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Fastcharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.xiaomi_redwood
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.redwood \
