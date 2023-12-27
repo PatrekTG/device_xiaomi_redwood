5210dab2a2d797425aaedbaab978e2ac4f8b485b
Gustavo Mendes <gusttavo.me@outlook.com>
2023-12-27 15:48:17 +0530

    redwood:  Use prebuilt mtdservice stack [1] 06-05 18:23:55.637 F/linker  (25460): CANNOT LINK EXECUTABLE "/vendor/bin/hw/vendor.qti.secure_element@1.2-service": library "vendor.xiaomi.hardware.mtdservice@1.0.so" not found: needed by /vendor/lib64/android.hardware.secure_element@1.0-impl.so in namespace (default)
    [2] 06-05 19:53:14.092 F/linker  (10224): CANNOT LINK EXECUTABLE "/vendor/bin/mlipayd@1.1": library "vendor.xiaomi.hardware.mlipay@1.0.so" not found: needed by main executable

--- device.mk
+++ device.mk
@@ -367,12 +367,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
-# Mlipay
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.0.vendor \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.0.vendor
-
 # Minijail
 PRODUCT_PACKAGES += \
     libavservices_minijail \
