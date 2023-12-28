e29584a23d6d0a489db953b4082ef0851437304e
thepriyanshujangid <priyanshujangid@yahoo.com>
2023-12-28 17:29:17 +0530

    redwood: stop shipping Gcam prebuilt

--- device.mk
+++ device.mk
@@ -584,7 +584,3 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
-
-# Gcam prebuilt
-PRODUCT_PACKAGES += \
-GCamGOPrebuilt-V4
